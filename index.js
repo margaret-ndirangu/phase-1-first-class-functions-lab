2da64a41d7fe44c70849f49fe7a5eea59320f30b
Margaret <wanjirundirangu43@gmail.com>
2022-06-10 11:27:37 +0300

    commit tree 4e314ff89ee285c88ae788b52c87df6cf1a9c470

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers= function(drivers) {
+    return drivers.slice(0,drivers.length -2)
+  };
+  const returnLastTwoDrivers= function(drivers) {
+    return drivers.slice(-2)
+  };
+  const selectingDrivers= [returnFirstTwoDrivers,returnLastTwoDrivers] 
+  const createFareMultiplier= function(value){ 
+    return function (fareMultiplier){
+      return (fareMultiplier*value);
+    }
+  } 
+  const fareDoubler= createFareMultiplier(2);
+  const fareTripler= createFareMultiplier(3) ; 
+ 
+  const selectDifferentDrivers= function(drivers,selectingDrivers){
+    return selectingDrivers(drivers);
+  }
+  
